27258ca3b84f45c406c2fbcbce36ae967f0fe308
P26pveilleux <144148685+P26pveilleux@users.noreply.github.com>
2023-09-20 14:37:28 -0400

    finsihed

---
+++ Lab_2-6+7.py
@@ -0,0 +1,47 @@
+# Create a Python file named lab_2-6.py
+
+# Set a = 5, b = 10, c = 0, d = -4
+a = 5
+b = 10
+c = 0
+d = -4 
+# Create a conditional statement to determine if each variable is even or odd. 
+if a/5 = int (a/5)
+print (a)
+# a is odd 
+if b/2 = int (b/5)
+print  (b)
+# b is evan 
+if c/5 = int (c/5)
+print (c)
+# c is odd 
+if d/5 = int (d/5)
+print (d)
+# d is odd 
+
+# Print the results.
+#a is less than 5 
+#b is equall to 5
+#c is less than 5
+#d is less than 5 
+
+# Lab_2-7.py
+
+if a < 5
+print (a is less than 5 )
+elif a > 5
+print (a is greater than 5 )
+if b < 5
+print (b is 5 less than 5 )
+elif b > 5 
+print (b is less thab 5)
+if c < 5 
+print (c is less than 5)
+elif c > 5 
+print (c is greater than 5)
+if d < 5
+print (d is less than 5)
+elif d > 5
+print (d is greater than 5 )
+
+
